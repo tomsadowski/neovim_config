61ac0bdaabbec282c8d2c0be0ee41d5093f6b39b
tomsadowski <tsadowski93@gmail.com>
2024-11-24 09:05:02 -0500

    add lua version of default

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.cmd('colorscheme daddy_chill')
+vim.cmd('colorscheme default_lua')
  
 -- options
 -- vim.o.cmdheight = 0
@@ -6,13 +6,13 @@ vim.o.number = true
 vim.o.laststatus = 1
 
 -- lsp c/cpp
-vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
-  pattern = {'*.c', '*.C', '*.h', '*.H', '*.cpp', '*.CPP', '*.hpp', '*.HPP'},
-  callback = function(args)
-    vim.lsp.start({
-      name = 'clang-lsp',
-      cmd = {'clangd'},
-      root_dir = vim.fs.root(args.buf, {'lsp_readme.txt'}),
-    })
-  end,
-})
+--vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+--  pattern = {'*.c', '*.C', '*.h', '*.H', '*.cpp', '*.CPP', '*.hpp', '*.HPP'}, -- file name pattern
+--  callback = function(args)
+--    vim.lsp.start({
+--      name = 'clang-lsp',
+--      cmd = {'clangd'},
+--      root_dir = vim.fs.root(args.buf, {'.git'}),
+--    })
+--  end,
+--})
