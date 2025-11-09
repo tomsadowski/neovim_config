3393d49545eab5c70005e824a22e81557835a73e
tomsadowski <tsadowski93@gmail.com>
2025-11-09 08:22:19 -0500

    fix diagnostic bg colors for moss

--- colors/moss.lua
+++ colors/moss.lua
@@ -9,13 +9,13 @@ require "tsad_colorscheme_main" {
 
     comment = {10,  10, "#506070"}, -- blue
 
-    warnbg  = { 1,   1, "#182010"}, -- darkgreen
+    warnbg  = { 1,   1, "#102010"}, -- darkgreen
     warn    = {10,  10, "#809040"}, -- green
     keyword = {10,  10, "#809040"}, -- green
 
     text    = {10,  10, "#b0a058"}, -- sand
 
-    errorbg = {10,  10, "#201810"}, -- darkclay
+    errorbg = {10,  10, "#201010"}, -- darkclay
     error   = {12,  12, "#b06858"}, -- clay
     func    = {12,  12, "#b06858"}, -- clay
     type    = { 9,   9, "#b06858"}, -- clay
