63f9ea0392b5ccf37c3a98b4f63123b9cbba05aa
tomsadowski <tsadowski93@gmail.com>
2025-11-09 20:06:20 -0500

    cyan yes

--- colors/cyan.lua
+++ colors/cyan.lua
@@ -10,13 +10,13 @@ require "tsad_colorscheme_main" {
     comment = {10,  10, "#006040"}, -- blue
 
     warnbg  = { 1,   1, "#102010"}, -- darkgreen
-    warn    = {10,  10, "#608060"}, -- green
-    keyword = {10,  10, "#608060"}, -- green
+    warn    = {10,  10, "#409040"}, -- green
+    keyword = {10,  10, "#409040"}, -- green
 
-    text    = {10,  10, "#a08060"}, -- sand
+    text    = {10,  10, "#a08040"}, -- sand
 
     errorbg = {10,  10, "#201010"}, -- darkclay
-    error   = {12,  12, "#a06080"}, -- clay
-    func    = {12,  12, "#a06080"}, -- clay
-    type    = { 9,   9, "#a06080"}, -- clay
+    error   = {12,  12, "#907090"}, -- clay
+    func    = {12,  12, "#907090"}, -- clay
+    type    = { 9,   9, "#907090"}, -- clay
 }
