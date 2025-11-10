b5a4130b6aedac5926ff2a29c256e3b3f2848c7f
tomsadowski <tsadowski93@gmail.com>
2025-11-10 12:55:33 -0500

    cyan improvments

--- colors/cyan.lua
+++ colors/cyan.lua
@@ -10,13 +10,14 @@ require "tsad_colorscheme_main" {
     comment = {10,  10, "#005050"}, -- blue
 
     warnbg  = { 1,   1, "#102010"}, -- darkgreen
-    warn    = {10,  10, "#40a040"}, -- green
-    keyword = {10,  10, "#40a040"}, -- green
+
+    warn    = {10,  10, "#609030"}, -- green
+    keyword = {10,  10, "#609030"}, -- green
 
     text    = {10,  10, "#b0a030"}, -- yellow
 
     errorbg = {10,  10, "#201010"}, -- darkclay
-    error   = {12,  12, "#a07890"}, -- clay
-    func    = {12,  12, "#a07890"}, -- clay
-    type    = { 9,   9, "#a07890"}, -- clay
+    error   = {12,  12, "#a07080"}, -- clay
+    func    = {12,  12, "#a07080"}, -- clay
+    type    = { 9,   9, "#a07080"}, -- clay
 }
