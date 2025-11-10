6e344baef5667c23b9b0fa4134c926d4befbbcf3
tomsadowski <tsadowski93@gmail.com>
2025-11-09 19:01:33 -0500

    wiffle

---
+++ colors/waffle.lua
@@ -0,0 +1,22 @@
+-- moss
+
+require "tsad_colorscheme_main" {
+    black   = { 4,   4, "#000000"}, -- black
+
+    canvas  = { 0,   0, "#081018"}, -- blue
+
+    visual  = { 2,   2, "#102020"}, -- cyan
+
+    comment = {10,  10, "#405060"}, -- blue
+
+    warnbg  = { 1,   1, "#102010"}, -- darkgreen
+    warn    = {10,  10, "#508018"}, -- green
+    keyword = {10,  10, "#508018"}, -- green
+
+    text    = {10,  10, "#908860"}, -- sand
+
+    errorbg = {10,  10, "#201010"}, -- darkclay
+    error   = {12,  12, "#a06080"}, -- clay
+    func    = {12,  12, "#a06080"}, -- clay
+    type    = { 9,   9, "#a06080"}, -- clay
+}
---
+++ colors/wiffle.lua
@@ -0,0 +1,22 @@
+-- wiffle
+
+require "tsad_colorscheme_main" {
+    black   = { 4,   4, "#000000"}, -- black
+
+    canvas  = { 0,   0, "#000020"}, -- blue
+
+    visual  = { 2,   2, "#002020"}, -- cyan
+
+    comment = {10,  10, "#004070"}, -- blue
+
+    warnbg  = { 1,   1, "#102010"}, -- darkgreen
+    warn    = {10,  10, "#208070"}, -- green
+    keyword = {10,  10, "#208070"}, -- green
+
+    text    = {10,  10, "#60a020"}, -- sand
+
+    errorbg = {10,  10, "#201010"}, -- darkclay
+    error   = {12,  12, "#a06070"}, -- clay
+    func    = {12,  12, "#a06070"}, -- clay
+    type    = { 9,   9, "#a06070"}, -- clay
+}
