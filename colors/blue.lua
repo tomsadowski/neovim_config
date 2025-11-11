840d6800609218bf9b4a1349c9061a471e5f0b98
tomsadowski <tsadowski93@gmail.com>
2025-11-11 14:08:57 -0500

    fixed blue

--- colors/blue.lua
+++ colors/blue.lua
@@ -7,16 +7,16 @@ require "tsad_colorscheme_main" {
 
     visual  = { 2,   2, "#002020"}, -- cyan
 
-    comment = {10,  10, "#004070"}, -- blue
+    comment = {10,  10, "#204080"}, -- blue
 
-    warnbg  = { 1,   1, "#102010"}, -- darkgreen
-    warn    = {10,  10, "#208070"}, -- green
-    keyword = {10,  10, "#208070"}, -- green
+    warnbg  = { 1,   1, "#102010"}, -- darkcyan
+    warn    = {10,  10, "#20b0b0"}, -- cyan
+    keyword = {10,  10, "#20b0b0"}, -- cyan
 
-    text    = {10,  10, "#60a020"}, -- sand
+    text    = {10,  10, "#c0d020"}, -- green
 
     errorbg = {10,  10, "#201010"}, -- darkclay
-    error   = {12,  12, "#a06070"}, -- clay
-    func    = {12,  12, "#a06070"}, -- clay
-    type    = { 9,   9, "#a06070"}, -- clay
+    error   = {12,  12, "#d09020"}, -- clay
+    func    = {12,  12, "#d09020"}, -- clay
+    type    = { 9,   9, "#d09020"}, -- clay
 }
