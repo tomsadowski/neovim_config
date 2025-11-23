3938d82977b10a17e5deecbe568aaec95183ba23
tomsadowski <tsadowski93@gmail.com>
2025-11-23 13:59:43 -0500

    bzzt

---
+++ colors/bzzt.lua
@@ -0,0 +1,30 @@
+-- bzzt
+
+require "tsad_colorscheme_main" {
+    black   = { 4,   4, "#000000"}, -- grey
+
+    canvas  = { 0,   0, "#101010"}, -- blue
+
+    lineno  = {10,  10, "#606060"}, -- grey
+    msgarea = {10,  10, "#606060"}, -- grey
+    comment = {10,  10, "#606060"}, -- blue
+
+
+    text    = {10,  10, "#c0b040"}, -- green
+    func    = {12,  12, "#c0b040"}, -- green
+    type    = { 9,   9, "#c0b040"}, -- green
+
+    warn    = {10,  10, "#40b0c0"}, -- pink
+    prim    = {10,  10, "#40b0c0"}, -- pink
+    num     = {10,  10, "#40b0c0"}, -- pink
+    delim   = {10,  10, "#40b0c0"}, -- pink
+    op      = {10,  10, "#40b0c0"}, -- pink
+    module  = {10,  10, "#40b0c0"}, -- pink
+
+    warnbg  = { 1,   1, "#303010"}, -- yellow
+    search  = { 4,   4, "#103010"}, -- yellow
+
+    error   = {12,  12, "#b040b0"}, -- pink
+    errorbg = {10,  10, "#301030"}, -- magenta
+    visual  = { 2,   2, "#301030"}, -- magenta
+}
