1d882ce87668e43225d226a19136764b1badab6e
tomsadowski <tsadowski93@gmail.com>
2025-11-24 12:30:18 -0500

    brush is something

--- colors/brush.lua
+++ colors/brush.lua
@@ -3,7 +3,7 @@
 require "tsad_colorscheme_main" {
     black   = { 0,   0, "#000000"}, -- black
 
-    canvas  = { 0, 233, "#201020"}, -- magenta
+    canvas  = { 0, 233, "#1a181a"}, -- magenta
 
     search  = { 6,  23, "#400040"}, -- magenta
     errorbg = { 3,  58, "#400040"}, -- magenta
@@ -11,10 +11,10 @@ require "tsad_colorscheme_main" {
     warnbg  = { 6,  23, "#003000"}, -- green
     visual  = { 3,  58, "#003000"}, -- green
 
-    lineno  = { 8,   8, "#604060"}, -- magenta
+    lineno  = { 8,   8, "#807080"}, -- magenta
 
-    msgarea = { 8,   8, "#904090"}, -- magenta
-    comment = { 8,   8, "#904090"}, -- magenta
+    msgarea = { 8,   8, "#807080"}, -- magenta
+    comment = { 8,   8, "#807080"}, -- magenta
 
     error   = {11, 184, "#b090f0"}, -- blue
     var     = {11, 184, "#b090f0"}, -- blue
@@ -22,6 +22,8 @@ require "tsad_colorscheme_main" {
     const   = {11, 184, "#b090f0"}, -- blue
     str     = {11, 184, "#b090f0"}, -- blue
     vbuilt  = {11, 184, "#b090f0"}, -- blue
+    num     = {14,  44, "#b090f0"}, -- blue
+    bool    = {14,  44, "#b090f0"}, -- blue
 
     warn    = {14,  44, "#f090b0"}, -- red
     text    = {14,  44, "#f090b0"}, -- red
@@ -31,7 +33,6 @@ require "tsad_colorscheme_main" {
     type    = {14,  44, "#f090b0"}, -- red
     member  = {14,  44, "#f090b0"}, -- red
     prim    = {14,  44, "#f090b0"}, -- red
-    num     = {14,  44, "#f090b0"}, -- red
     delim   = {14,  44, "#f090b0"}, -- red
     op      = {14,  44, "#f090b0"}, -- red
     module  = {14,  44, "#f090b0"}, -- red
