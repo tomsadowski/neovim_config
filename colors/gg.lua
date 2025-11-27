e02d09698ded69b9fc6f48b0eb4a478bfc37c37b
tomsadowski <tsadowski93@gmail.com>
2025-11-27 09:52:17 -0500

    gggg

--- colors/gg.lua
+++ colors/gg.lua
@@ -6,11 +6,12 @@ require "tsad_colorscheme_main" {
   canvas     = { 0, 233, "#000000"}, -- black
 
   -- normal
-  primary    = {11, 184, "#a0a080"}, -- white
-  letdecl    = {11, 184, "#a0a080"}, -- white
-  enumdecl   = {11, 184, "#a0a080"}, -- white
-  path       = {11, 184, "#a0a080"}, -- white
-  curlineno  = { 8,   8, "#a0a080"}, -- white
+  primary    = {11, 184, "#b0b080"}, -- white
+  letdecl    = {11, 184, "#b0b080"}, -- white
+  enumdecl   = {11, 184, "#b0b080"}, -- white
+  path       = {11, 184, "#b0b080"}, -- white
+  curlineno  = { 8,   8, "#b0b080"}, -- white
+  msgarea    = { 8,   8, "#b0b080"}, -- white
 
   -- alternate
   secondary  = {11, 184, "#50a0e0"}, -- blue
@@ -23,15 +24,14 @@ require "tsad_colorscheme_main" {
   bool       = {11, 184, "#d05050"}, -- red
 
   -- literal
-  str        = {11, 184, "#50b050"}, -- green
-  literal    = {11, 184, "#50b050"}, -- green
+  str        = {11, 184, "#30b030"}, -- green
+  literal    = {11, 184, "#30b030"}, -- green
 
   -- peripheral
   peripheral = { 8,   8, "#606060"}, -- grey
   comment    = { 8,   8, "#606060"}, -- grey
-  lineno     = {11, 184, "#606060"}, -- magenta
+  lineno     = {11, 184, "#606060"}, -- grey
 
-  msgarea    = { 8,   8, "#c040c0"}, -- magenta
   matchparen = {11, 184, "#c040c0"}, -- magenta
   func       = {11, 184, "#c040c0"}, -- magenta
   enum       = {11, 184, "#c040c0"}, -- magenta
