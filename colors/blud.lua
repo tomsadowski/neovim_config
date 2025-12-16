9122d870de47b6957159c758e7d447c912279c58
tomsadowski <tsadowski93@gmail.com>
2025-12-15 20:55:19 -0500

    another

--- colors/blud.lua
+++ colors/blud.lua
@@ -6,13 +6,17 @@ local darkcyan    = { 6,  23, "#305040"}
 local darkblue    = { 4,  17, "#304050"}
 local darkmagenta = { 5,  53, "#403050"}
 
-local darkgrey    = { 0, 234, "#000000"}
-local grey        = { 8, 241, "#707070"}
+local darkgrey    = { 0, 234, "#202020"}
+local grey        = { 8, 241, "#808080"}
 
-local red         = { 9, 205, "#f070b0"}
-local yellow      = {11, 215, "#f0b070"}
-local blue        = {12,  75, "#70b0f0"}
-local magenta     = {10, 155, "#b070f0"}
+local red         = { 9, 205, "#e070a0"}
+local blue        = {12,  75, "#70a0e0"}
+
+local yellow      = {11, 215, "#d0b070"}
+local green       = {10, 155, "#90d090"}
+
+--local magenta     = { 8, 241, "#a080e0"}
+local magenta     = { 8, 241, "#70a0e0"}
 
 
 
@@ -35,18 +39,18 @@ require "tsad-colorscheme-main" {
   path       = yellow, 
   uselist    = yellow, 
 
-  variable   = magenta, 
-  letdecl    = magenta, 
-  patdecl    = magenta, 
-  param      = magenta, 
-  args       = magenta, 
-  secondary  = magenta, 
-  curlineno  = magenta, 
+  variable   = green, 
+  letdecl    = green, 
+  patdecl    = green, 
+  param      = green, 
+  args       = green, 
+  secondary  = green, 
+  curlineno  = green, 
 
-  str        = blue, 
-  matchparen = blue, 
-  literal    = blue, 
-  bool       = blue, 
+  str        = magenta, 
+  matchparen = magenta, 
+  literal    = magenta, 
+  bool       = magenta, 
 
   keyword    = blue,
 
