311b2843f48fe82f695948619d5b6811c4e38b27
tomsadowski <tsadowski93@gmail.com>
2026-01-05 15:11:40 -0500

    handsome

--- colors/tom.lua
+++ colors/tom.lua
@@ -1,17 +1,17 @@
 -- tom
 
 local p = vim.deepcopy(require "palette")
-p.black[3]   = "#181818"
+p.black[3]   = "#101010"
 
 p.dred[3]    = "#402020"
 p.dblue[3]   = "#202040"
 
-p.dgrey[3]   = "#808080"
+p.dgrey[3]   = "#606060"
 p.grey[3]    = "#c0c0c0"
 
-p.yellow[3]  = "#a0c020"
-p.magenta[3] = "#e07080"
-p.cyan[3]    = "#70a0d0"
+p.yellow[3]  = "#80c000"
+p.magenta[3] = "#d06070"
+p.cyan[3]    = "#60a0c0"
 
 require "tsad-colorscheme-main" {
   canvas     = p.black, 
