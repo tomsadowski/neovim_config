65bb710253a20181cdfd9b20379987d0f85b3f9f
tomsadowski <tsadowski93@gmail.com>
2026-02-01 22:33:59 -0500

    gumgreen

---
+++ colors/carrot_in_pool.lua
@@ -0,0 +1,44 @@
+-- carrot in pool
+
+local p = vim.deepcopy(require "palette")
+
+  p.black[3]    = "#000810"
+  p.dgrey[3]    = "#204060"
+  p.white[3]    = "#b0c0d0"
+
+  p.dcyan[3]    = "#002020"
+  p.dyellow[3]  = "#202000"
+  p.dmagenta[3] = "#200020"
+
+--p.cyan[3]     = "#00a0a0"
+  p.cyan[3]     = "#00a0c0"
+--p.yellow[3]   = "#a0c050"
+  p.yellow[3]   = "#60c020"
+--  p.magenta[3]  = "#d070a0"
+  p.magenta[3]  = "#e08040"
+
+require "tsad-colorscheme-main" {
+  canvas     = p.black, 
+
+  search     = p.dyellow, 
+  visual     = p.dmagenta, 
+  diagund    = p.dcyan,
+
+  msgarea    = p.dgrey, 
+  lineno     = p.dgrey, 
+  comment    = p.dgrey, 
+
+  normal     = p.white, 
+  curlineno  = p.white, 
+
+  matchparen = p.magenta, 
+  type       = p.magenta, 
+  func       = p.magenta, 
+
+  keyword    = p.cyan,
+  literal    = p.cyan, 
+  str        = p.cyan, 
+
+  variable   = p.yellow, 
+  btype      = p.yellow, 
+}
---
+++ colors/gumgreen.lua
@@ -0,0 +1,44 @@
+-- carrot in pool
+
+local p = vim.deepcopy(require "palette")
+
+  p.black[3]    = "#000810"
+  p.dgrey[3]    = "#204060"
+  p.white[3]    = "#a0b0c0"
+
+  p.dcyan[3]    = "#002020"
+  p.dyellow[3]  = "#202000"
+  p.dmagenta[3] = "#200020"
+
+--p.cyan[3]     = "#00a0a0"
+  p.cyan[3]     = "#7070c0"
+--p.yellow[3]   = "#a0c050"
+  p.yellow[3]   = "#70a000"
+--  p.magenta[3]  = "#d070a0"
+  p.magenta[3]  = "#d06080"
+
+require "tsad-colorscheme-main" {
+  canvas     = p.black, 
+
+  search     = p.dyellow, 
+  visual     = p.dmagenta, 
+  diagund    = p.dcyan,
+
+  msgarea    = p.dgrey, 
+  lineno     = p.dgrey, 
+  comment    = p.dgrey, 
+
+  normal     = p.white, 
+  curlineno  = p.white, 
+
+  matchparen = p.magenta, 
+  type       = p.magenta, 
+  func       = p.magenta, 
+
+  keyword    = p.cyan,
+  literal    = p.cyan, 
+  str        = p.cyan, 
+
+  variable   = p.yellow, 
+  btype      = p.yellow, 
+}
