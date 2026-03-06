8d0c17865a49a513f8f77babd0b8f156cd49472c
tomsadowski <tsadowski93@gmail.com>
2026-03-05 22:49:54 -0500

    the

--- colors/0init.lua
+++ colors/0init.lua
@@ -2,38 +2,38 @@
 
 local p = vim.deepcopy(require "palette")
 
-  p.dred[3]     = "#400000"
-  p.dmagenta[3] = "#400040"
-  p.dcyan[3]    = "#004040"
-  p.dyellow[3]  = "#404000"
+  p.dred[3]     = "#402020"
+  p.dmagenta[3] = "#402040"
+  p.dcyan[3]    = "#103040"
+  p.dgreen[3]   = "#104020"
 
   p.black[3]    = "#0a0602"
-  p.dgrey[3]    = "#605040"
-  p.white[3]    = "#d0a070"
+  p.dyellow[3]  = "#605040"
+  p.yellow[3]   = "#d0a070"
 
-  p.yellow[3]   = "#60e0a0"
+  p.green[3]    = "#60e0a0"
   p.cyan[3]     = "#80a0f0"
-  p.magenta[3]  = "#e080e0"
+  p.magenta[3]  = "#d090f0"
   p.red[3]      = "#d05060"
 
 require "tsad-colorscheme-main" {
   canvas     = p.black, 
 
   search     = p.dcyan, 
-  visual     = p.dyellow, 
+  visual     = p.dgreen, 
   diagund    = p.dred,
 
-  msgarea    = p.dgrey, 
-  lineno     = p.dgrey, 
-  comment    = p.dgrey, 
+  msgarea    = p.dyellow, 
+  lineno     = p.dyellow, 
+  comment    = p.dyellow, 
 
-  normal     = p.white, 
-  curlineno  = p.white, 
+  normal     = p.yellow, 
+  curlineno  = p.yellow, 
 
-  item       = p.yellow, 
-  matchparen = p.yellow, 
-  type       = p.yellow, 
-  func       = p.yellow, 
+  item       = p.green, 
+  matchparen = p.green, 
+  type       = p.green, 
+  func       = p.green, 
 
   keyword    = p.red,
 
