a8fb9357ec7db38228044ff4e0e837a3020adeec
tomsadowski <tsadowski93@gmail.com>
2026-03-15 19:02:41 -0400

    this christmas

--- colors/0init.lua
+++ colors/0init.lua
@@ -4,17 +4,17 @@ local c = require "color"
 local p = vim.deepcopy(c.palette)
 
   p.dyellow.g  = "#64605c"
-  p.yellow.g   = "#c0b8b0"
+  p.yellow.g   = "#b8b0a8"
 
   p.dblue.g    = "#303030"
   p.dgreen.g   = "#183018"
   p.dcyan.g    = "#103040"
   p.dmagenta.g = "#402030"
 
-  p.red.g      = "#c06030"
-  p.green.g    = "#90b000"
-  p.cyan.g     = "#60a0f0"
-  p.magenta.g  = "#c060e0"
+  p.red.g      = "#905030"
+  p.green.g    = "#70a000"
+  p.cyan.g     = "#6090c0"
+  p.magenta.g  = "#a060d0"
 
 c.apply {
   canvas     = p.black, 
