5d896c7ddd08c2c05a209e789cd90e7e5aac15a4
tomsadowski <tsadowski93@gmail.com>
2026-03-21 09:28:11 -0400

    a

--- pack/tsad/opt/tsadcolor/colors/nature.lua
+++ pack/tsad/opt/tsadcolor/colors/nature.lua
@@ -3,12 +3,11 @@
 require "treesitter"
 local c = require "color"
 local p = vim.deepcopy(c.palette)
-local p2 = vim.deepcopy(c.palette)
 
   p.black.g    = "#131211"
   p.dblue.g    = "#202020"
   p.dgrey.g    = "#403c38"
-  p.grey.g     = "#686460"
+  p.grey.g     = "#666462"
   p.white.g    = "#c8c4c0"
 
   p.dyellow.g  = "#283008"
@@ -16,9 +15,8 @@ local p2 = vim.deepcopy(c.palette)
 
   p.red.g      = "#a07040"
   p.yellow.g   = "#60b020"
-  p.cyan.g     = "#40a0f0"
- -- p.magenta.g  = "#d090a0"
-  p.magenta.g  = "#c060a0"
+  p.cyan.g     = "#50a0f0"
+  p.magenta.g  = "#c070c0"
 
 c.apply {
   canvas     = p.black, 
