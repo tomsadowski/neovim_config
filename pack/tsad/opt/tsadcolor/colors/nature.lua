9bf9e32596c4df9343d5095cba7f6e344f292b14
tomsadowski <tsadowski93@gmail.com>
2026-03-21 10:55:29 -0400

    day

--- pack/tsad/opt/tsadcolor/colors/nature.lua
+++ pack/tsad/opt/tsadcolor/colors/nature.lua
@@ -8,15 +8,15 @@ local p2 = vim.deepcopy(c.palette)
   p.black.g    = "#202020"
   p.dblue.g    = "#303030"
   p.dgrey.g    = "#404040"
-  p.grey.g     = "#707070"
-  p.white.g    = "#d0d0d0"
+  p.grey.g     = "#706c68"
+  p.white.g    = "#d0c8c0"
 
   p.dyellow.g  = "#283008"
   p.dcyan.g    = "#103040"
 
   p.red.g      = "#d08040"
   p.yellow.g   = "#70c020"
-  p.cyan.g     = "#50c0ff"
+  p.cyan.g     = "#40c0f0"
   p.magenta.g  = "#e070e0"
 
 c.apply {
