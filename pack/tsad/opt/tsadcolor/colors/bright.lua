6463d7a6226e8f37c20a5076a7e552921ce8ab3e
tomsadowski <tsadowski93@gmail.com>
2026-03-24 22:03:58 -0400

    meh

--- pack/tsad/opt/tsadcolor/colors/bright.lua
+++ pack/tsad/opt/tsadcolor/colors/bright.lua
@@ -5,7 +5,7 @@ local c = require "color"
 local p = vim.deepcopy(c.palette)
 local p2 = vim.deepcopy(c.palette)
 
-  p.black.g    = "#303030"
+  p.black.g    = "#101010"
   p.dblue.g    = "#404040"
   p.dgrey.g    = "#505050"
   p.grey.g     = "#808080"
