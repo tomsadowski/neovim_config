a3f6a3555f7b332d5e0677f07197ca64aa70648b
tomsadowski <tsadowski93@gmail.com>
2026-03-26 10:38:12 -0400

    pinker magenta

--- pack/tsad/opt/tsadcolor/colors/current.lua
+++ pack/tsad/opt/tsadcolor/colors/current.lua
@@ -17,7 +17,7 @@ local p2 = vim.deepcopy(c.palette)
   p.red.g      = "#c08060"
   p.yellow.g   = "#90c040"
   p.cyan.g     = "#70c0e0"
-  p.magenta.g  = "#e090e0"
+  p.magenta.g  = "#e090c0"
 
 c.apply {
   canvas     = p.black, 
